52d4382037242de8dbe9526557b94c26e8851131
AL Bashkir <41126916+al-bashkir@users.noreply.github.com>
2020-02-14 12:08:59 +0000

    add sh file

---
+++ shavukha.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+status_code=$(curl -s -o /dev/null -w "%{http_code}" 'https://eda.yandex/restaurant/nasha_shaurma_krasnopresnenskaya_naberezhnaya_16s1')
+last_status=123
+stamp=$(date '+[%F %H:%M]')
+echo "${stamp} Script started" >> /tmp/shawarma.log
+
+while $(sleep 1); do
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" 'https://eda.yandex/restaurant/nasha_shaurma_krasnopresnenskaya_naberezhnaya_16s1')
+        if [[ ${status_code} -eq 200 ]]; then
+            if [[ ${status_code} -ne ${last_status} ]]; then
+                stamp=$(date '+[%F %H:%M]')
+                echo "${stamp} Open" >> /tmp/shawarma.log
+                /usr/bin/notify-send --urgency=critical "Мрази за баксы и Анюта ждут твой ротик."
+                last_status=${status_code}
+            fi
+            elif [[ ${status_code} -eq 302 ]]; then
+                if [[ ${status_code} -ne ${last_status} ]]; then
+                    stamp=$(date '+[%F %H:%M]')
+                    echo "${stamp} Closed" >> /tmp/shawarma.log
+                    /usr/bin/notify-send --urgency=low "Все, Анюта занята."
+                    last_status=${status_code}
+                fi
+            fi
+done
